de4fe1557fe5964084a39cb450423770a4648797
Martin Norbury <martin.norbury@gmail.com>
2015-03-04 14:18:24 -0800

    Add a simple hello world script.

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+"""
+hello_world.py - Simple script to say hello.
+
+Author:
+    Martin Norbury (martin.norbury@gmail.com)
+
+March 2015
+"""
+def say_hello():
+    print 'Hello World'
+
+if __name__ == '__main__':
+    say_hello()
